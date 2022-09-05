70f8f70fa9eadfac59d2809b17d42c0f987b970f
Leandro Mesa <leandro.mesa@isaportal.uy>
2022-09-05 17:52:08 -0300

    paragraphs, bloques, layout builder and dev mode

---
+++ dev_mode.sh
@@ -0,0 +1,9 @@
+echo " >> Limpiando cache"
+
+echo " >> Vaciando configuraciones a archivo files"
+
+echo " >> Instalando modulos de desarrollo"
+
+echo " >> Copiando archivos de depuracion"
+
+echo " >> Limpiando cache"
