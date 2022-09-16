47de7b86c749e07c21a407b5d5f0778a014a2059
Leandro Mesa <leandro.mesa@isaportal.uy>
2022-09-16 18:20:51 -0300

    Ajustes a paginas de diseno, servicios y modulos

--- web/profiles/custom/portal_express/updates/updates.php
+++ web/profiles/custom/portal_express/updates/updates.php
@@ -21,15 +21,31 @@ function portal_express_update_9002(&$sandbox)
 }
 
 /**
- * Implements hook_update_9003().
+ * Implements hook_update_9004 ().
+ *
+ * Update layout page and layout builder
  */
-function portal_express_update_9003(&$sandbox)
+function portal_express_update_9004(&$sandbox)
 {
   _portal_express_import_config([
-    'isa_field_storage', 'isa_core'
+    'isa_field_storage', 'isa_layout_page', 'isa_layout_builder'
   ]);
 
   \Drupal::service('module_installer')->install([
-    'isa_core'
+    'isa_layout_page', 'isa_layout_builder'
+  ]);
+}
+
+/**
+ * Implements hook_update_N().
+ */
+function portal_express_update_9005(&$sandbox)
+{
+  _portal_express_import_config([
+    'isa_layout_page'
+  ]);
+
+  \Drupal::service('module_installer')->install([
+    'isa_layout_page'
   ]);
 }
